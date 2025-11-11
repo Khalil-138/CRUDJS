67e9fbcaa9d70f7dcafe3b15dd26f6a464a087ad
Khalil <khalilsantana92@gmail.com>
2025-11-10 22:09:26 -0300

    prod1

---
+++ src/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+import ServiceUser from '../service/users.js'
+const JWT_SEGREGO = "S3GR3D0"
+
+export default async function authMiddleware(req, res, next) {
+
+    try {
+        const token = req.headers['authorization']
+
+
+        if (!token) {
+            throw new Error("Você não tem permissão para realizar esta ação")
+        }
+
+        const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREGO)
+
+        const user = await ServiceUser.FindOne(decoded.id)
+
+
+
+        req.headers.user = user
+        next()
+
+    } catch (error) {
+        res.status(403).send({
+            data: null,
+            msg: error.message,
+            error: true
+        })
+
+    }
+}
+
+
