ab81c4a707efceb3bfbf0f83394c1efb9b099f09
Khalil <khalilsantana92@gmail.com>
2025-11-12 21:37:16 -0300

    commit1

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,33 +2,35 @@ import jwt from 'jsonwebtoken'
 import ServiceUser from '../service/users.js'
 const JWT_SEGREGO = "S3GR3D0"
 
-export default async function authMiddleware(req, res, next) {
+export default function authMiddleware(roles = []) {
+    return async (req, res, next) => {
 
-    try {
-        const token = req.headers['authorization']
+        try {
+            const token = req.headers['authorization']
+            if (!token) {
+                throw new Error("Você não tem permissão para realizar esta ação")
+            }
+            const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREGO)
+            const user = await ServiceUser.FindOne(decoded.id)
+            if(
+                roles.length &&
+                !roles.includes(user.permissao)
+            ) {
+                throw new Error("você não tem permissao para realizar esta ação")
+            }
 
+            req.headers.user = user
 
-        if (!token) {
-            throw new Error("Você não tem permissão para realizar esta ação")
+            next()
+        } catch (error) {
+            res.status(403).send({
+                data: null,
+                msg: error.message,
+                error: true
+            })
+
         }
-
-        const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREGO)
-
-        const user = await ServiceUser.FindOne(decoded.id)
-
-
-
-        req.headers.user = user
-        next()
-
-    } catch (error) {
-        res.status(403).send({
-            data: null,
-            msg: error.message,
-            error: true
-        })
-
     }
-}
 
+}
 
